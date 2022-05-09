163cd58d608a1c4ef12062a141cc775324683705
Abdullah AYGÜN <abdullah.aygun@via.tc>
2022-05-09 21:22:03 +0300

    Context ve ServiceRegistration eklendi.

--- Quiz/Quiz.API/Program.cs
+++ Quiz/Quiz.API/Program.cs
@@ -1,11 +1,14 @@
+using Quiz.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbServices();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().SetIsOriginAllowed(x => true)));
 
 var app = builder.Build();
 
@@ -15,9 +18,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors();
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
-
+if (app.Environment.IsProduction())
+{
+    var port = Environment.GetEnvironmentVariable("PORT");
+    app.Urls.Add($"http://*:{port}");
+}
 app.Run();
---
+++ Quiz/Quiz.Data/ServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Data
+{
+    public static class ServiceRegistration
+    {
+        public static void AddDbServices(this IServiceCollection services)
+        {
+            ConfigurationManager configurationManager = new();
+            configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory()));
+            configurationManager.AddJsonFile("appsettings.json");
+            // App deki interface, Persistence deki class
+            //services.AddSingleton<>();
+            services.AddDbContext<QuizDbContext>(options => options.UseNpgsql(configurationManager.GetConnectionString("PostgreSQL")));
+        }
+    }
+}
